06d34f3ee8c3a318be933fc8b626af7110d45308
Arul15 <arulpandiyan@Sybrants-Mac-mini-2.local>
2015-03-12 14:41:07 +0530

    Initial Commit

---
+++ Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animation
+//
+//  Created by Arulpandiyan on 12/03/15.
+//  Copyright (c) 2015 Sybrant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Animation
+//
+//  Created by Arulpandiyan on 12/03/15.
+//  Copyright (c) 2015 Sybrant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
